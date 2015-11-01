95f66362f46ddc35ef160087c970942eef598318
RegularSvensson <elias.svensson.1992@gmail.com>
2015-11-01 13:47:03 +0100

    Remove old printf:s

--- mario.c
+++ mario.c
@@ -9,8 +9,4 @@ int main(void)
         printf("Please give me a non-negative integer: ");
         height = GetInt();
     } while(height <= 0 || height > 23);
-    printf("You chose: %d\n", height);
-    printf("Hello git!\n");
-    printf("You did it!\n");
-    printf("#\n");
 }
