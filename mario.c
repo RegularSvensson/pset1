b2c986ee846fa8d1636f2e0a0e2b9059f4a562c8
RegularSvensson <elias.svensson.1992@gmail.com>
2015-11-01 13:32:25 +0100

    Add mario files

---
+++ mario.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    int height;
+    do
+    {
+        printf("Please give me a non-negative integer: ");
+        height = GetInt();
+    } while(height <= 0 || height > 23);
+    printf("You chose: %d\n", height);
+}
