22a6084b08a8a0be2fce3d8bc7bc0a7a59ff649a
RegularSvensson <elias.svensson.1992@gmail.com>
2015-11-01 13:47:17 +0100

    Print out square of #'s

--- mario.c
+++ mario.c
@@ -9,4 +9,12 @@ int main(void)
         printf("Please give me a non-negative integer: ");
         height = GetInt();
     } while(height <= 0 || height > 23);
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < height; j++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
 }
